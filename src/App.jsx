5c6e72c4df2f58f1e6f13684525e31ade5076495
Vedant Gulhane <vedantgulhane24@gmail.com>
2026-03-27 16:05:17 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import Family from './Pages/Family/Family';
+
+const App = () => {
+  return (
+    <div>
+    <Family/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Family/Family.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Family.css';
+
+const Family = () => {
+  return (
+    <div className='mainClass'>
+      <div><p className='innerText'>Sunil Gulhane</p></div>
+      <div><p className='innerText'>Varsha Gulhane</p></div>
+     <div><p className='innerText'> Vedant Gulhane</p></div>
+     <div><p className='innerText'> Siddhant Gulhane</p> </div>
+    </div>
+  )
+}
+
+export default Family
